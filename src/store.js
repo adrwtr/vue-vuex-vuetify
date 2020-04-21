a691542b9e61375f5d3bf028941cd280120f05cb
Adriano Waltrick <akuma.majunia@gmail.com>
2020-04-21 08:22:33 -0300

    Exemplo basico de axios

--- src/store.js
+++ src/store.js
@@ -1,17 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex, axios)
 
 const store = new Vuex.Store({
     state : {
-        count : 1
+        count : 1,
+        arrPosts : []
+    },
+
+    actions : {
+        loadTest({commit}) {
+            console.log('aqui 2');
+            axios.get(
+                    'https://jsonplaceholder.typicode.com/todos/1'
+                ).then(
+                    data => {
+                        console.log(data.data);
+                        let arrValores = data.data;
+                        commit('setArrPost', arrValores);
+                    }
+                ).catch(
+                    error => {
+                        console.log(error);
+                    }
+                )
+        }
     },
 
     mutations : {
         setCount(state, payload) {
             state.count = payload;
-
+        },
+        setArrPost(state, payload) {
+            state.arrPosts = payload;
         }
     }
 });
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,17 +1,30 @@
 <template>
-    <div>Index page</div>
+    <div>
+        Index page
+        <br>
+        <h1 v-for="(obj, index) in arrPosts" :key="index">
+            {{ obj }}
+        </h1>
+    </div>
 </template>
 
 <script>
+    import {mapState} from 'vuex';
+
     export default {
-        props: {
-            source: String,
-        },
         data: () => ({
             drawer: null,
         }),
         created () {
-            this.$vuetify.theme.dark = true
+            this.$vuetify.theme.dark = true;
+            this.$store.dispatch('loadTest');
+        },
+        computed: {
+            ...mapState(
+                [
+                    'arrPosts'
+                ]
+            )
         },
     }
 </script>
